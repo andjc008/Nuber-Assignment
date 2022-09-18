bc2f7d67325d31ef9d6aa01d93f9c396f12ee097
andjc008 <100395631+andjc008@users.noreply.github.com>
2022-09-18 14:26:33 +0930

    file upload base level implementation

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "s3nuberassignmentstorage3a699220": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -8,15 +8,33 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
+import { Storage } from "@aws-amplify/storage"
+
+
 
 function App({ signOut }) {
+
+
+  async function onChangeFunction(e) {
+    if (!e.target.files[0]) return
+    //Clear bucket
+    const file = e.target.files[0];
+    await Storage.put(file.name, file, {
+      level: "private"
+    });
+  }
+
+
+
+
   return (
     <View className="App">
       <Card>
         <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
+        <Heading level={1}>Logged In</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
+      <input type="file" onChange={onChangeFunction}></input>
     </View>
   );
 }
